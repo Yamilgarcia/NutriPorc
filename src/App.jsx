2b9a6154843c317cb1d2b40bc2dd3ce2bd6e4415
Yamil <Yamilg620@gmail.com>
2026-06-29 15:21:37 -0600

    limpieza de rama monitoreo peso ia

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import AppLayout from "./layout/AppLayout";
-import WelcomePage from "./pages/WelcomePage";
-import InsumosPage from "./pages/InsumosPage";
-import LotesPage from "./pages/LotesPage";
 import PesajesPage from "./pages/PesajesPage";
 //EJEMPLO DE COMO DEBEN IR LAS RUTAS
 
@@ -10,9 +7,7 @@ export default function App() {
   return (
     <Routes>
       <Route element={<AppLayout />}>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/insumos" element={<InsumosPage />} />
-        <Route path="/lotes" element={<LotesPage />} />
+
         <Route path="/monitoreo" element={<PesajesPage />} />
       </Route>
     </Routes>
--- src/pages/PesajesPage.jsx
+++
@@ -1,2 +0,0 @@
-import Pesajes from "../features/monitoreoIA/ui/PesajesPage";
-export default function PesajesPage() { return <section><Pesajes /></section>; }
