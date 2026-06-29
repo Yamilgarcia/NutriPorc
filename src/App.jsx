59ba59dfe5f7d0708682e351c6f7aaf383d23201
Yamil <Yamilg620@gmail.com>
2026-06-29 15:18:44 -0600

    Limpieza de maximizador

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import AppLayout from "./layout/AppLayout";
 import WelcomePage from "./pages/WelcomePage";
-import InsumosPage from "./pages/InsumosPage";
-import LotesPage from "./pages/LotesPage";
-import PesajesPage from "./pages/PesajesPage";
-import FormuladorPage from "./features/formulador/ui/FormuladorPage";
 import MaximizadorPage from "./features/maximizador/ui/MaximizadorPage";
 
 export default function App() {
@@ -12,10 +8,7 @@ export default function App() {
     <Routes>
       <Route element={<AppLayout />}>
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/insumos" element={<InsumosPage />} />
-        <Route path="/lotes" element={<LotesPage />} />
-        <Route path="/monitoreo" element={<PesajesPage />} />
-        <Route path="/formulador" element={<FormuladorPage />} />
+ 
         <Route path="/maximizador" element={<MaximizadorPage />} />
       </Route>
     </Routes>
