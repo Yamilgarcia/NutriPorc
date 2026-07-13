d8b6c6168c1b25fef8116e6d89147e57cca2fb9b
Yamil <Yamilg620@gmail.com>
2026-07-12 22:57:31 -0600

    chore: actualiza rutas y layout principal con nuevo modulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Form } from "react-router-dom";
 import AppLayout from "./layout/AppLayout";
 import WelcomePage from "./pages/WelcomePage";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -9,7 +9,8 @@ import MaximizadorPage from "./features/maximizador/ui/MaximizadorPage";
 import PesajesPage from "./pages/PesajesPage";
 import FormuladorPage from "./features/formulador/ui/FormuladorPage";
 import Insumos from "./features/insumos/ui/InsumosPage";
-
+import { DashboardFinanciero } from "./features/finanzas/ui/DashboardFinanciero";  
+import Semaforo from "./features/semaforo/ui/SemaforoPage.jsx";
 // Atrapamos el parámetro antes de que React Router redireccione y lo borre
 if (window.location.search.includes("pago=exitoso")) {
   localStorage.setItem("pago_pendiente_procesar", "true");
@@ -17,11 +18,25 @@ if (window.location.search.includes("pago=exitoso")) {
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route path="/" element={<WelcomePage />} />
-       
-      </Route>
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        {/* RUTA PÚBLICA */}
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* RUTAS PROTEGIDAS: Requieren autenticación */}
+        <Route element={<ProtectedRoute />}>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<WelcomePage />} />
+            <Route path="/lotes" element={<LotesPage />} />
+            <Route path="/maximizador" element={<MaximizadorPage />} />
+            <Route path="/monitoreo" element={<PesajesPage />} />
+            <Route path="/formulador" element={<FormuladorPage />} />
+            <Route path="/insumos" element={<Insumos />} />
+            <Route path="/finanzas" element={<DashboardFinanciero />} />
+            <Route path="/Semaforo" element={<Semaforo />} />
+          </Route>
+        </Route>
+      </Routes>
+    </AuthProvider>
   );
 }
